828f21b2727540f44d9a4d725723c3abb103859a
Maya <mayabe@users.noreply.github.com>
2015-11-29 14:37:19 -0800

    Adding game_result column to teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -15,6 +15,7 @@
 
 class Team < ActiveRecord::Base
   enum color: [:red, :blue, :green]
+  enum game_result: [:win, :loss]
   has_many :users, :foreign_key => "current_team_id"
   belongs_to :current_user, :class_name => "User"
   belongs_to :next_user, :class_name => "User"
