e477da35395a786f86d463ea017886798827eb6e
Anthony Rodriguez <arodr101@mail.sfsu.edu>
2015-12-02 18:46:04 -0800

    add token and remove token

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -46,6 +46,20 @@ class GameController < ApplicationController
     render :json => response
   end
 
+  # user leaves
+  def in_game_leave
+    board = Board.find(params[:board_id])
+    user = User.find(params[:user_id])
+
+    board.teams.each do |team|
+      team.users.each do |each_user|
+        each_user.current_team = nil
+        each_user.status = :lobby
+        each_user.save
+      end
+    end
+  end
+
   # notify that passed in user is ready
   # user_id
   # channel_name
@@ -80,15 +94,81 @@ class GameController < ApplicationController
     render :json => response
   end
 
+  # add a token to the board
+  #   board_id
+  #   user_id
+  #   position (token)
+  #   card (token)
+  #   channel_name (pusher channel)
+  #   public_update_event_name (pusher event)
+  #   user_update_event_name (pusher event)
   def add_token
+    board = Board.find(params[:board_id])
+    user = User.find(params[:user_id])
+
+    if board.can_add_token?(params[:position])
+      user.add_token(params[:card], params[:position])
+    end
+
+    draw(board, user)
+
+    push_public_board_info(params[:channel_name], params[:public_update_event_name], board)
+    push_user_hand_info(params[:channel_name], params[:user_update_event_name], user)
 
   end
 
+  # add a token to the board
+  #   board_id
+  #   user_id
+  #   position (token)
+  #   card (token)
+  #   channel_name (pusher channel)
+  #   public_update_event_name (pusher event)
+  #   user_update_event_name (pusher event)
   def remove_token
+    board = Board.find(params[:board_id])
+    user = User.find(params[:user_id])
 
+    if user.can_remove_token(params[:card], params[:position])
+      if board.remove_token(params[:position])
+        user.hand.delete(params[:card])
+      end
+    end
   end
 
   protected
+
+  def discard(user, card)
+    position = user.hand.index(card)
+    if position != nil
+      @board.last_discard = user.hand.delete_at(position)
+      @board.save
+      user.save
+    end
+  end
+
+  def draw(board, user)
+    # if we want to deal from the top of the deck rather than the end, we can change this
+    if board.deck.empty?
+      shuffleDeck
+    end
+    user.hand.push(board.deck.pop)
+    board.save
+    user.save
+  end
+
+  def shuffleDeck
+    @board.deck = (1..104).to_a.shuffle
+    @board.teams.each do |team|
+      team.users.each do |user|
+        user.hand.each do |card|
+          position = @board.deck.index(card)
+          @board.deck.delete_at(position)
+          @board.save
+        end
+      end
+    end
+  end
 
   def json_request?
     request.format.json?
@@ -120,8 +200,8 @@ class GameController < ApplicationController
   def push_user_hand_info(channel_name, event_name, user)
     user_json = []
     user_json.push(
-            {:user_id => user.id, :username => user.username, :avatar => user.avatar, :current_team_id => user.current_team, :hand => user.hand}
-        )
+        {:user_id => user.id, :username => user.username, :avatar => user.avatar, :current_team_id => user.current_team, :hand => user.hand}
+    )
     Pusher[channel_name].trigger(event_name, user_json)
   end
 
