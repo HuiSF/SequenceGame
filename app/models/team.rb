5ba648bc6825c92a63b89c129f66b101a62fa060
ztgeng <zhtgeng@gmail.com>
2015-12-07 07:25:02 -0800

    finish helper methods for check sequences, needs test

--- app/models/team.rb
+++ app/models/team.rb
@@ -29,36 +29,72 @@ class Team < ActiveRecord::Base
     end
     self.sequence_in_row(token, positions)
     self.sequence_in_column(token, positions)
-    self.sequence_in_diagonal(token, positions)
+    self.sequence_in_diagonal_tl_br(token, positions)
+    self.sequence_in_diagonal_tr_bl(token, positions)
   end
 
 
   protected
 
   def sequence_in_row(token, positions)
-    row_pos = token.to_i % 10
+    # sequence can use ONE of the tokens in existed sequence
+    use_existed_token = false
+    interrupt_other_sequence = false
+    row_pos = (token.to_i - 1) % 10 + 1
     potential_sequence = [token]
 
     (1..(row_pos - 1)).each do |x|
       pos = token.to_i - x
       if(positions.include?(pos))
+        self.sequences.each do |sequence|
+          if sequence.include? pos
+            if use_existed_token
+              # Trying to use two tokens in existed sequences
+              interrupt_other_sequence = true
+            else
+              use_existed_token = true
+            end
+            break
+          end
+        end
+        # Using two tokens in existed sequences is illegal
+        if interrupt_other_sequence
+          break
+        end
         potential_sequence.push(pos.to_s)
       else
         break
       end
-      if potential_sequence.length == 5
+      if potential_sequence.length > 4
         break
       end
     end
 
+    # reset interrupt_other_sequence for second half of growing
+    interrupt_other_sequence = false
+
     (1..(10 - row_pos)).each do |x|
-      pos = token.to_i + x
-      if(positions.include?(pos))
-        potential_sequence.push(pos.to_s)
-      else
+      # check length of potential sequence BEFORE growing
+      if potential_sequence.length > 4
         break
       end
-      if potential_sequence.length == 5
+      pos = token.to_i + x
+      if(positions.include?(pos))
+        self.sequences.each do |sequence|
+          if sequence.include? pos
+            if use_existed_token
+              interrupt_other_sequence = true
+            else
+              use_existed_token = true
+            end
+            break
+          end
+        end
+        if interrupt_other_sequence
+          break
+        end
+        potential_sequence.push(pos.to_s)
+      else
         break
       end
     end
@@ -70,11 +106,205 @@ class Team < ActiveRecord::Base
   end
 
   def sequence_in_column(token, positions)
-    # TODO
+    use_existed_token = false
+    interrupt_other_sequence = false
+    col_pos = (token.to_i - 1) / 10 + 1
+    potential_sequence = [token]
+
+    (1..(col_pos - 1)).each do |x|
+      pos = token.to_i - x * 10
+      if(positions.include?(pos))
+        self.sequences.each do |sequence|
+          if sequence.include? pos
+            if use_existed_token
+              interrupt_other_sequence = true
+            else
+              use_existed_token = true
+            end
+            break
+          end
+        end
+        if interrupt_other_sequence
+          break
+        end
+        potential_sequence.push(pos.to_s)
+      else
+        break
+      end
+      if potential_sequence.length > 4
+        break
+      end
+    end
+
+    interrupt_other_sequence = false
+
+    (1..(10 - col_pos)).each do |x|
+      if potential_sequence.length > 4
+        break
+      end
+      pos = token.to_i + x * 10
+      if(positions.include?(pos))
+        self.sequences.each do |sequence|
+          if sequence.include? pos
+            if use_existed_token
+              interrupt_other_sequence = true
+            else
+              use_existed_token = true
+            end
+            break
+          end
+        end
+        if interrupt_other_sequence
+          break
+        end
+        potential_sequence.push(pos.to_s)
+      else
+        break
+      end
+    end
+    # if at least 5 in a row, process sequence and possibly add it to sequences
+    # else do nothing
+    if potential_sequence.length > 4
+      self.process_potential_sequence(potential_sequence)
+    end
   end
 
-  def sequence_in_diagonal(token, positions)
-    # TODO
+  def sequence_in_diagonal_tl_br(token, positions)
+    use_existed_token = false
+    interrupt_other_sequence = false
+    row_pos = (token.to_i - 1) % 10 + 1
+    potential_sequence = [token]
+
+    (1..(row_pos - 1)).each do |x|
+      pos = token.to_i - x * 11
+      if pos < 1
+        break
+      end
+      if(positions.include?(pos))
+        self.sequences.each do |sequence|
+          if sequence.include? pos
+            if use_existed_token
+              interrupt_other_sequence = true
+            else
+              use_existed_token = true
+            end
+            break
+          end
+        end
+        if interrupt_other_sequence
+          break
+        end
+        potential_sequence.push(pos.to_s)
+      else
+        break
+      end
+      if potential_sequence.length > 4
+        break
+      end
+    end
+
+    interrupt_other_sequence = false
+    (1..(10 - row_pos)).each do |x|
+      if potential_sequence.length > 4
+        break
+      end
+      pos = token.to_i + x * 11
+      if pos > 100
+        break
+      end
+      if(positions.include?(pos))
+        self.sequences.each do |sequence|
+          if sequence.include? pos
+            if use_existed_token
+              interrupt_other_sequence = true
+            else
+              use_existed_token = true
+            end
+            break
+          end
+        end
+        if interrupt_other_sequence
+          break
+        end
+        potential_sequence.push(pos.to_s)
+      else
+        break
+      end
+    end
+    # if at least 5 in a row, process sequence and possibly add it to sequences
+    # else do nothing
+    if potential_sequence.length > 4
+      self.process_potential_sequence(potential_sequence)
+    end
+  end
+
+  def sequence_in_diagonal_tr_bl(token, positions)
+    use_existed_token = false
+    interrupt_other_sequence = false
+    row_pos = (token.to_i - 1) % 10 + 1
+    potential_sequence = [token]
+
+    (1..(row_pos - 1)).each do |x|
+      pos = token.to_i - x * 9
+      if pos < 1
+        break
+      end
+      if(positions.include?(pos))
+        self.sequences.each do |sequence|
+          if sequence.include? pos
+            if use_existed_token
+              interrupt_other_sequence = true
+            else
+              use_existed_token = true
+            end
+            break
+          end
+        end
+        if interrupt_other_sequence
+          break
+        end
+        potential_sequence.push(pos.to_s)
+      else
+        break
+      end
+      if potential_sequence.length > 4
+        break
+      end
+    end
+
+    interrupt_other_sequence = false
+    (1..(10 - row_pos)).each do |x|
+      if potential_sequence.length > 4
+        break
+      end
+      pos = token.to_i + x * 9
+      if pos > 100
+        break
+      end
+      if(positions.include?(pos))
+        self.sequences.each do |sequence|
+          if sequence.include? pos
+            if use_existed_token
+              interrupt_other_sequence = true
+            else
+              use_existed_token = true
+            end
+            break
+          end
+        end
+        if interrupt_other_sequence
+          break
+        end
+        potential_sequence.push(pos.to_s)
+      else
+        break
+      end
+    end
+    # if at least 5 in a row, process sequence and possibly add it to sequences
+    # else do nothing
+    if potential_sequence.length > 4
+      self.process_potential_sequence(potential_sequence)
+    end
   end
 
   def process_potential_sequence(potential_sequence)
